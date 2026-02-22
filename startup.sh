8ab4aabbb6804f66ff8ff3348e24b241f2571f93
blankazselyke <kissblanka02@gmail.com>
2026-02-22 17:48:28 +0100

    Update

---
+++ startup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+python manage.py migrate --noinput
+
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin123')" | python manage.py shell
+
+gunicorn -b :$PORT myproject.wsgi
